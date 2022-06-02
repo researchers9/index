74ae8a4f0b342c6e54c2290949c9fb16c4da1026
researchers9 <106528117+researchers9@users.noreply.github.com>
2022-06-02 21:00:49 +0800

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,182 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A tool with a flattened, cross-shaped, or star-shaped tip that fits into the head of a screw to turn it",
+    answer: "Screw Driver",
+    options: [
+      "Screw Driver",
+      "Cable Ties",
+      "Anti-Static wrist strap",
+      "Multitester"
+    ]
+  },
+    {
+    numb: 2,
+    question: "______________ straps are designed to safely ground operators working with ESD sensitive components, preventing a build-up of static electricity.",
+    answer: "Anti-Static wrist strap",
+    options: [
+      "Screw Driver",
+      "Cable Ties",
+      "Anti-Static wrist strap",
+      "Multitester"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A fastener consisting of a thin, flexible nylon strap with a notched surface, one end of which is threaded through a locking mechanism at the other. It can be used to manage the cables in system unit properly.",
+    answer: "Cable Ties",
+    options: [
+      "Screw Driver",
+      "Cable Ties",
+      "Anti-Static wrist strap",
+      "Multitester"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A _____________ is a measuring instrument that can measure multiple electrical properties.",
+    answer: "Multitester",
+    options: [
+      "Screw Driver",
+      "Cable Ties",
+      "Anti-Static wrist strap",
+      "Multitester"
+    ]
+  },
+    {
+    numb: 5,
+    question: "It is the first step in disassembling system unit",
+    answer: "Unplug your computer and peripheral items",
+    options: [
+      "Remove side covers",
+      "Remove motherboard",
+      "Remove Power Supply Unit",
+      "Unplug your computer and peripheral items"
+    ]
+  },
+  {
+    numb: 6,
+    question: "It is the last step in disassembling system unit",
+    answer: "Remove motherboard",
+    options: [
+      "Remove side covers",
+      "Remove motherboard",
+      "Remove Power Supply Unit",
+      "Unplug your computer and peripheral items"
+    ]
+  },
+    {
+    numb: 7,
+    question: "It is a hand held multipurpose tool used to cut and remove coat of wires and join materials or components by pressing them together.",
+    answer: "Crimping Tool",
+    options: [
+      "LAN Tester",
+      "Crimping Tool",
+      "Anti-static mat",
+      "Multitmeter"
+    ]
+  },
+    {
+    numb: 8,
+    question: "It is the first step in assembling system unit",
+    answer: "Put back the motherboard",
+    options: [
+      "Put back the motherboard",
+      "Install the memory",
+      "Install CPU fan",
+      "Put back the side cover"
+    ]
+  },
+    {
+    numb: 9,
+    question: "A _____________ is the main printed circuit board (PCB) in a computer.",
+    answer: "Motherboard",
+    options: [
+      "Random Access Memory",
+      "Processor",
+      "Motherboard",
+      "Optical Disc Drive"
+    ]
+  },
+    {
+    numb: 10,
+    question: "An ____________ in a computer system allows you to use CDs, DVDs, and Blu-ray discs to listen to music or watch a movie.",
+    answer: "Optical Disc Drive",
+    options: [
+      "Random Access Memory",
+      "Processor",
+      "Motherboard",
+      "Optical Disc Drive"
+    ]
+  },
+   {
+    numb: 11,
+    question: "It is a storage device that uses integrated circuit assemblies to store data persistently, typically using flash memory, and functioning as secondary storage in the hierarchy of computer storage",
+    answer: "Solid State Drive",
+    options: [
+      "Solid State Drive",
+      "Random Access Memory",
+      "Power Supply",
+      "Hard Disk Drive"
+    ]
+  },
+    {
+    numb: 12,
+    question: "This is a type of screwdriver that has pointed edges in the shape of a cross.",
+    answer: "Philip screwdriver",
+    options: [
+      "Hex Screwdriver",
+      "Torx screwdriver",
+      "Flat head screwdriver",
+      "Philip screwdriver"
+    ]
+  },
+    {
+    numb: 13,
+    question: "This is a type of screwdriver that is tar-shaped and typically used in vehicles, motorcycles, bicycles, computer systems, hard disk drives and consumer electronics.",
+    answer: "Torx screwdriver",
+    options: [
+      "Hex Screwdriver",
+      "Torx screwdriver",
+      "Flat head screwdriver",
+      "Philip screwdriver"
+    ]
+  },
+    {
+    numb: 14,
+    question: "____________ is a form of computer memory that can be read and changed",
+    answer: "Random Access Memory",
+    options: [
+      "Random Access Memory",
+      "Processor",
+      "Hard Disk",
+      "Optical Disc Drive"
+    ]
+  },
+    {
+    numb: 15,
+    question: "A magnetic storage medium for a computer. Hard disks are flat circular plates made of aluminum or glass and coated with a magnetic material.",
+    answer: "Hard Disk",
+    options: [
+      "Random Access Memory",
+      "Processor",
+      "Hard Disk",
+      "Optical Disc Drive"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
